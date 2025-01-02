55bd3713c96defe466cbe2b81592192aa507e120
Vignesh Kadam <149602803+kadamvignesh@users.noreply.github.com>
2025-01-02 23:21:20 +0530

    Create userdata1.sh

---
+++ userdata1.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+apt update
+apt install -y apache2
+
+
+apt install -y awscli
+
+cat <<EOF > /var/www/html/index.html
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Portfolio</title>
+  <style>
+    /* Add animation and styling for the text */
+    @keyframes colorChange {
+      0% { color: red; }
+      50% { color: green; }
+      100% { color: blue; }
+    }
+    h1 {
+      animation: colorChange 2s infinite;
+    }
+  </style>
+</head>
+<body>
+  <h1>Terraform Project 2</h1>
+  <h1>Best football club is FC Barcelona </h1>
+
+</body>
+</html>
+EOF
+
+# Start Apache and enable it on boot
+systemctl start apache2
+systemctl enable apache2
